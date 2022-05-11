f1c367e4851b65453f301f0b41649cedba59ff78
Cleve <carsten.cleve@realcore.de>
2022-05-11 08:44:39 +0200

    Changed app_Location

---
+++ DynamicCV/Pages/Curriculum.razor.cs
@@ -0,0 +1,27 @@
+﻿using DynamicCV.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DynamicCV.Pages
+{
+    public partial class Curriculum
+    {
+        [Inject]
+        public HttpClient Http { get; set;}
+        private Person? person;
+
+        protected override async Task OnInitializedAsync()
+        {
+            person = await Http.GetFromJsonAsync<Person>("sample-data/person.json");
+            //person.ProjectHistoryEntries[0].StartingDate = DateTime.Now;
+            string jsonString = JsonSerializer.Serialize(person);
+        }
+
+    }
+}
